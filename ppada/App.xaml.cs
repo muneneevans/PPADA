bc0a5b387d9c1afa432535e724e5fc285b3416a6
evans <munene.evansk@gmail.com>
2018-01-04 10:54:54 +0300

    [ADD] : Start page

--- ppada/App.xaml.cs
+++ ppada/App.xaml.cs
@@ -125,7 +125,7 @@ namespace ppada
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter.
-                if (!rootFrame.Navigate(typeof(Home), e.Arguments))
+                if (!rootFrame.Navigate(typeof(Start), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
